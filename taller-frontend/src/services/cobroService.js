1e8eacbe4d56f5a493e336348d08b1780183e237
Guillermo <guillermoardonalfaro@gmail.com>
2026-06-14 15:54:06 -0600

    correcion archivo cobrosService.js

--- taller-frontend/src/services/cobroService.js
+++
@@ -1,7 +0,0 @@
-import axiosInstance from '../api/axiosInstance';
-
-export const registrarCobro = (data) => axiosInstance.post('/cobros/registrar', data);
-export const getArqueoDiario = () => axiosInstance.get('/cobros/arqueo');
-export const getHistorial = (params) => axiosInstance.get('/cobros/historial', { params });
-export const getServicios = () => axiosInstance.get('/cobros/servicios');
-    
